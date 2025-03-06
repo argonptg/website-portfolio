f1b94e8f8d93a14dded391e9d0b965154ae9658b
Davi Machado <brisolo@proton.me>
2025-03-06 20:13:55 -0300

    add i18n, english posts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import mdx from "@astrojs/mdx";
-
 // https://astro.build/config
 export default defineConfig({
     site: "https://argonptg.neocities.org",
-    integrations: [mdx()],
+    integrations: [],
 });
---
+++ src/i18n/ui.ts
@@ -0,0 +1,21 @@
+export const languages = [
+    "English",
+    "Portugues"
+];
+
+export const defaultLanguage = "pt";
+
+export const ui = {
+    en: {
+        "nav.projects": "projects",
+        "nav.about": "about",
+        "nav.blog": "blog",
+        "content.back": "back",
+    },
+    pt: {
+        "nav.projects": "projetos",
+        "nav.about": "sobre",
+        "nav.blog": "blog",
+        "content.back": "voltar",
+    }
+} as const;
